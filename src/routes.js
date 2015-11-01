e33dea3d05e011bc96ff0343622335caaa1a1bee
Jose Tavares <josetavares00@gmail.com>
2015-11-01 16:05:49 +0000

    add handlebars template engine

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,6 @@
 
 module.exports = function (app) {
     app.get('/', function (req, res) {
-        res.sendFile(__dirname + '/index.html');
+        res.render('home', {defaultGreeting: process.env.DEFAULTGREETING});
     });
 };
--- src/server.js
+++ src/server.js
@@ -4,11 +4,14 @@
 
 function herokuDemo() {
     var express = require('express');
+    var exphbs  = require('express-handlebars');
     var port = process.env.PORT || 8080;
 
     var app = global.app = express();
 
     app.set('port', port);
+    app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+    app.set('view engine', 'handlebars');
 
     //Create routes and controllers
     require('./routes')(app);
